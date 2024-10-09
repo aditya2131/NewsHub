f7dc11b6711845d5880860dbbefb56e66fd11f26
Aditya Mishra <100842818+aditya2131@users.noreply.github.com>
2024-10-09 20:05:31 +0530

    added proptypes for validation

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,21 +1,35 @@
 // NewsList.js
 import React from 'react';
+import PropTypes from 'prop-types';
 import NewsItem from './NewsItem';
 import '../styles/NewsList.css';
 
-const NewsList = ({ articles }) => {
+const NewsList = ({ articles, loading }) => {
   // Check if articles is an array and has elements
+  if (loading) {
+    return <div>Loading articles...</div>; // Show loading message
+  }
+
   if (!Array.isArray(articles) || articles.length === 0) {
     return <div>No articles available</div>; // Show a message if there are no articles
   }
 
   return (
     <div className="news-list">
-      {articles.map((article, index) => (
-        <NewsItem key={index} article={article} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} /> // Use a unique key
       ))}
     </div>
   );
+};
+
+NewsList.propTypes = {
+  articles: PropTypes.array.isRequired,
+  loading: PropTypes.bool, // Optional loading prop
+};
+
+NewsList.defaultProps = {
+  loading: false, // Default value for loading prop
 };
 
 export default NewsList;
